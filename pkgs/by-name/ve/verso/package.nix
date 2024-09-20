a51e6bd03b10c123cc7f7006d67c32a9a0200805
supinie <86788874+supinie@users.noreply.github.com>
2024-09-20 11:07:41 +0100

    testing against fork

--- pkgs/by-name/ve/verso/package.nix
+++ pkgs/by-name/ve/verso/package.nix
@@ -49,25 +49,26 @@ rustPlatform.buildRustPackage {
   version = "0-unstable-2024-09-11";
 
   src = fetchFromGitHub {
-    owner = "versotile-org";
+    owner = "supinie";
     repo = "verso";
-    rev = "1427d3f9059ea1e7f03b5bffe6b50cf0765fae14";
-    hash = "sha256-daFjpPvhoOE4qwKbVlyB7yt0qHdZSKE7lJLKVCo94z0=";
+    rev = "992b26d6475e1dbfafd988013cacd8407c9e250c";
+    hash = "sha256-4Vo5d3iyoGpWSHAeAN+D3ewSeXXlDdAMN2nbAGc7w2I=";
   };
 
   # git dependencies so need to include `Cargo.lock`
   cargoLock = {
     lockFile = ./Cargo.lock;
     outputHashes = {
-      "background_hang_monitor-0.0.1" = "sha256-WJ6Dt9iZLjROj3k7+pXSf+0SjQDwexFBAd0dmptfvVI=";
+      "background_hang_monitor-0.0.1" = lib.fakeHash;
       "derive_common-0.0.1" = "sha256-WHgQkZ5rY21qApg9LBHNBExn7T7A412lfMR45wKoTH4=";
       "fontsan-0.5.2" = "sha256-4id66xxQ8iu0+OvJKH77WYPUE0eoVa9oUHmr6lRFPa8=";
-      "mozjs-0.14.1" = "sha256-RMM28Rd0r58VLfNEJzjWw3Ze6oKEi5lC1Edv03tJbfY=";
-      "naga-22.0.0" = "sha256-Xi2lWZCv4V2mUbQmwV1aw3pcvIIcyltKvv/C+LVqqDI=";
-      "peek-poke-0.3.0" = "sha256-WCZYX68vZrPhaAZwpx9/lUp3bVsLMwtmlJSW8wNb2ks=";
+      "mozjs-0.14.1" = lib.fakeHash;
+      "naga-22.0.0" = lib.fakeHash;
+      "peek-poke-0.3.0" = lib.fakeHash;
       "servo-media-0.1.0" = "sha256-+J/6ZJPM9eus6YHJA6ENJD63CBiJTtKZdfORq9n6Nf8=";
       "signpost-0.1.0" = "sha256-xRVXwW3Gynace9Yk5r1q7xA60yy6xhC5wLAyMJ6rPRs=";
-      "webxr-0.0.1" = "sha256-E25HBvOnRre0pZBwUt3RJQmk9rmhiEHZFeQpjZVqfAY=";
+      "webxr-0.0.1" = lib.fakeHash;
+      "size_of_test" = lib.fakeHash;
     };
   };
 
