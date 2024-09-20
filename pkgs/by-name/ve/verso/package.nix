dfc71293a63b5fcfe7a629b746e9ea85996672f7
supinie <86788874+supinie@users.noreply.github.com>
2024-09-20 10:32:58 +0100

    working build failing runtime

--- pkgs/by-name/ve/verso/package.nix
+++ pkgs/by-name/ve/verso/package.nix
@@ -27,8 +27,22 @@
   wayland,
   libGL,
   gnumake,
-  mold
+  mold,
+  makeWrapper
 }:
+
+let
+  runtimePaths = lib.makeLibraryPath [
+    zlib
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXi
+    libxkbcommon
+    vulkan-loader
+    wayland
+    libGL
+  ];
+in
 
 rustPlatform.buildRustPackage {
   pname = "verso";
@@ -46,32 +60,36 @@ rustPlatform.buildRustPackage {
     lockFile = ./Cargo.lock;
     outputHashes = {
       "background_hang_monitor-0.0.1" = "sha256-WJ6Dt9iZLjROj3k7+pXSf+0SjQDwexFBAd0dmptfvVI=";
-      "derive_common-0.0.1" = "sha256-z0I2fQQlbUqaFU1EX45eYDy5IbZJ4SIget7WHzq4St0=";
+      "derive_common-0.0.1" = "sha256-WHgQkZ5rY21qApg9LBHNBExn7T7A412lfMR45wKoTH4=";
       "fontsan-0.5.2" = "sha256-4id66xxQ8iu0+OvJKH77WYPUE0eoVa9oUHmr6lRFPa8=";
       "mozjs-0.14.1" = "sha256-RMM28Rd0r58VLfNEJzjWw3Ze6oKEi5lC1Edv03tJbfY=";
       "naga-22.0.0" = "sha256-Xi2lWZCv4V2mUbQmwV1aw3pcvIIcyltKvv/C+LVqqDI=";
       "peek-poke-0.3.0" = "sha256-WCZYX68vZrPhaAZwpx9/lUp3bVsLMwtmlJSW8wNb2ks=";
-      "raqote-0.8.5" = "sha256-WLsz5q08VNmYBxUhQ0hOn0K0RVFnnjaWF/MuQGkO/Rg=";
       "servo-media-0.1.0" = "sha256-+J/6ZJPM9eus6YHJA6ENJD63CBiJTtKZdfORq9n6Nf8=";
       "signpost-0.1.0" = "sha256-xRVXwW3Gynace9Yk5r1q7xA60yy6xhC5wLAyMJ6rPRs=";
-      "webxr-0.0.1" = "sha256-HZ8oWm5BaBLBXo4dS2CbWjpExry7dzeB2ddRLh7+98w=";
+      "webxr-0.0.1" = "sha256-E25HBvOnRre0pZBwUt3RJQmk9rmhiEHZFeQpjZVqfAY=";
     };
   };
 
   RUSTC_BOOTSTRAP = true;
 
+  # remap relative path to fit nix-build environment
+  # remap path between modules to include SEMVER
+  # set `HOME` to a temp dir for write access
+  # fix invalid option errors during linking (https://github.com/mozilla/nixpkgs-mozilla/commit/c72ff151a3e25f14182569679ed4cd22ef352328)
   configurePhase = ''
-    ls -al /build/source
     sed -i -e 's/..\/..\/..\/resources\//\/build\/source\/resources\//g' ../cargo-vendor-dir/embedder_traits-0.0.1/resources.rs
     sed -i -e 's/\/style\//\/style-0.0.1\//g' ../cargo-vendor-dir/servo_atoms-0.0.1/build.rs
+    sed -i -e 's/SERVO_ROOT = os.path.abspath(.*)/SERVO_ROOT = os.path.abspath("\/build\/source")/g' ../cargo-vendor-dir/script-0.0.1/dom/bindings/codegen/run.py
 
     export HOME=$TMPDIR
+    unset AS
   '';
 
   nativeBuildInputs = [
     llvmPackages.bintools
     llvmPackages.llvm
-    llvmPackages.libclang
+    llvmPackages.libstdcxxClang
     udev
     cmake
     dbus
@@ -85,6 +103,7 @@ rustPlatform.buildRustPackage {
     m4
     gnumake
     mold
+    makeWrapper
     (python3.withPackages (ps: with ps; [pip dbus mako]))
   ];
 
@@ -103,21 +122,22 @@ rustPlatform.buildRustPackage {
     wayland
   ];
 
-  cargoBuildHook = ''
-    export RUSTFLAGS="-C cfg=servo_production"
-    cargo build --release
+  postFixup = ''
+    wrapProgram $out/bin/verso \
+      --prefix LD_LIBRARY_PATH : ${runtimePaths}
   '';
 
-  LD_LIBRARY_PATH = lib.makeLibraryPath [
-    zlib
-    xorg.libXcursor
-    xorg.libXrandr
-    xorg.libXi
-    libxkbcommon
-    vulkan-loader
-    wayland
-    libGL
-  ];
-
   LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
+  RUST_BACKTRACE = 1;
+
+  doCheck = false;
+
+  meta = {
+    description = "The embeddable, independent, memory-safe, modular, parallel web rendering engine";
+    homepage = "https://servo.org";
+    license = lib.licenses.mpl20;
+    maintainers = with lib.maintainers; [ supinie ];
+    mainProgram = "verso";
+    platforms = lib.platforms.linux ++ lib.platforms.darwin;
+  };
 }
